ac8b715d36182b21c5b9cb0ba72e37e47480a686
PieroGz <129896854+PieroGz@users.noreply.github.com>
2024-07-05 19:22:51 -0500

    Delete ConversorDeMonedas directory

--- ConversorDeMonedas/src/com/aluracursos/conversordemonedas/modelo/Moneda.java
+++
@@ -1,8 +0,0 @@
-package com.aluracursos.conversordemonedas.modelo;
-
-public record Moneda(double conversion_result) {
-    @Override
-    public double conversion_result() {
-        return conversion_result;
-    }
-}
